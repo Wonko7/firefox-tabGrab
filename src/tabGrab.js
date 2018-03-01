25d0d64c4c1a7309efbd903f687843c9704d5d90
William <William@undefined.re>
2018-03-01 12:04:13 +0100

    basic grab works

--- src/tabGrab.js
+++ src/tabGrab.js
@@ -1,20 +1,39 @@
-function listTabs() {
-  getCurrentWindowTabs().then((tabs) => {
-    const tabsList    = document.getElementById('tabs-list')
-    const currentTabs = document.createDocumentFragment()
-      .filter(t => t.pinned)
-      // .sort((t1, t2) => t1.index <= t2.index)
+const listTabs = () =>  browser.tabs.query({ currentWindow: true })
+  .then(tabs => tabs.filter(t => !t.pinned))
 
-    currentTabs.map(t => console.log('tab: ', t.index))
+
+const getCurrent = () =>  browser.tabs.query({ currentWindow: true, active: true })
+  .then(tabs => browser.tabs.get(tabs[0].id))
+
+function grab(search, count) {
+  let   tabs    = listTabs()
+  const re      = new RegExp(search.replace(/\\/, '\\\\'))
+  const pred    = tab => (tab.title.match(re) || tab.url.match(re))
+  const currTab = getCurrent()
+
+  console.log(`${re} vs ${search}`)
+
+  if (count > 0) {
+    tabs = tabs.slice(0, count)
+  }
+
+  Promise.all([currTab, tabs]).then(([currTab, tabs]) =>  {
+    const toMove  = tabs.filter(pred).map(t => t.id)
+    console.log(currTab)
+    console.log(toMove)
+    browser.tabs.move(toMove, { index: currTab.index })
   })
 }
 
+//   browser.runtime.onMessage.addListener((message) => {
+//     if (message.command === "beastify") {
+//       insertBeast(message.beastURL);
+//     } else if (message.command === "reset") {
+//       removeExistingBeasts();
+//     }
 
-function callOnActiveTab(tab, tabs) {
-  let index = 0;
-  console.log(`moving ${tab.id} to ${index}`)
-  browser.tabs.move([tab.id], {index});
-}
+console.log('heyyyyyy lmao 2')
 
-
-listTabs()
+grab('slashdot', 0)
+  .then(x => console.log('yes, got x:', x))
+  .resolve()
