9dbabf643b01196a1dfd38eb04f2d9fce6fe14cc
William <William@undefined.re>
2018-03-01 14:29:16 +0100

    rm debug

--- src/grabUi.js
+++ src/grabUi.js
@@ -1,6 +1,6 @@
 browser.runtime.onMessage.addListener((message, sender, sendResp) => {
   if (message.command === 'get-search') {
-    const search = window.prompt('lol')
+    const search = window.prompt('Grab tabs matching:')
     sendResp(search)
   }
 })
--- src/tabGrab.js
+++ src/tabGrab.js
@@ -1,33 +1,26 @@
+// Helper functions:
 const listTabs = () =>  browser.tabs.query({ currentWindow: true })
   .then(tabs => tabs.filter(t => !t.pinned))
-
 
 const getCurrent = () =>  browser.tabs.query({ currentWindow: true, active: true })
   .then(tabs => browser.tabs.get(tabs[0].id))
 
-function grab(search, count) {
+// grab all tabs matching search string and group them after current tab:
+function grab(search, countOrDirectionUnused) {
   let   tabs    = listTabs()
   const re      = new RegExp(search.replace(/\\/, '\\\\'))
   const pred    = tab => (tab.title.match(re) || tab.url.match(re))
   const currTab = getCurrent()
 
-  console.log(`${re} vs ${search}`)
-
-  if (count > 0) {
-    tabs = tabs.slice(0, count)
-  }
-
-  Promise.all([currTab, tabs]).then(([currTab, tabs]) =>  {
+  Promise.all([currTab, tabs]).then(([currTab, tabs]) => {
     const toMove  = tabs.filter(pred).map(t => t.id)
-    console.log(currTab)
-    console.log(toMove)
     browser.tabs.move(toMove, { index: currTab.index })
   })
 }
 
 browser.commands.onCommand.addListener((command) => {
   const currTab   = getCurrent()
-  const direction = command === 'grab-down' ? 1 : -1
+  const direction = command === 'grab-down' ? 1 : -1 // for now just grabs all.
 
   currTab
     .then(t    => browser.tabs.sendMessage(t.id, { command: 'get-search' }))
