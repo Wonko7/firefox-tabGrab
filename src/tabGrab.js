8dfbd024d729b0e025737e0eca8fc001e6a81185
William <William@undefined.re>
2018-03-03 12:25:33 +0100

    not sure if this is better

--- src/tabGrab.js
+++ src/tabGrab.js
@@ -1,19 +1,19 @@
 /* global browser */
 
 // Helper functions:
-const listTabs = () =>  browser.tabs.query({ currentWindow: true })
+const listTabs = () => browser.tabs.query({ currentWindow: true })
   .then(tabs => tabs.filter(t => !t.pinned))
 
-const getCurrent = () =>  browser.tabs.query({ currentWindow: true, active: true })
+const getCurrent = () => browser.tabs.query({ currentWindow: true, active: true })
   .then(tabs => browser.tabs.get(tabs[0].id))
 
 // grab all tabs matching search string and group them after current tab:
-const grab = async (search, countOrDirectionUnused) => {
-  const re                 = new RegExp(search.replace(/\\/, '\\\\'), 'i') // need to test this, maybe seperate by \s? "slash dot" => title.match(slash).match(dot)?
-  const rePredicate        = tab => (tab.title.match(re) || tab.url.match(re))
-  const [allTabs, currTab] = await Promise.all([listTabs(), getCurrent()])
-  const tabsToMove         = allTabs.filter(rePredicate)
-  const nbTabsBeforeCurr   = tabsToMove.filter(t => t.index < currTab.index).length
+const grab = async (currTab, search, countOrDirectionUnused) => {
+  const re               = new RegExp(search.replace(/\\/, '\\\\'), 'i') // need to test this, maybe seperate by \s? "slash dot" => title.match(slash).match(dot)?
+  const rePredicate      = tab => (tab.title.match(re) || tab.url.match(re))
+  const allTabs          = await listTabs()
+  const tabsToMove       = allTabs.filter(rePredicate)
+  const nbTabsBeforeCurr = tabsToMove.filter(t => t.index < currTab.index).length
 
   await browser.tabs.move(tabsToMove.map(t => t.id), { index: currTab.index })
   await browser.tabs.move(currTab.id, { index: currTab.index - nbTabsBeforeCurr })
@@ -24,6 +24,7 @@ browser.commands.onCommand.addListener((command) => {
   const direction = command === 'grab-down' ? 1 : -1 // for now just grabs all.
 
   return currTab
-    .then(t    => browser.tabs.sendMessage(t.id, { command: 'get-search' }))
-    .then(resp => grab(resp, direction))
+    .then(t => [t, browser.tabs.sendMessage(t.id, { command: 'get-search' })])
+    .then(Promise.all.bind(Promise))
+    .then(([currTab, response]) => grab(currTab, response, direction))
 })
